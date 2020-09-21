e42a4746c8939240079f96ff42acfb5d50c02f59
rizary <andika.riyan@gmail.com>
2020-09-21 13:35:56 +0700

    implement devshell and flake

--- pkgs/rust/default.nix
+++ pkgs/rust/default.nix
@@ -1,8 +1,8 @@
 { rustChannelOf }:
- (rustChannelOf {
-                date = "2019-09-13";
-                channel = "nightly";
-                sha256 = "06881g7ba2hzmfq5vaz888d2q762zf4bxjc621rw3g8z702ps7w9";
+(rustChannelOf {
+  date = "2019-09-13";
+  channel = "nightly";
+  sha256 = "06881g7ba2hzmfq5vaz888d2q762zf4bxjc621rw3g8z702ps7w9";
 }).rust.override {
   extensions = [
     "clippy-preview"
